793b08fa9134a1f310f7ffe03a0a5b27f76fde18
Dmitriy Sim <dmitriy.sim89@gmail.com>
2026-07-25 11:34:46 +0300

    ValueSource задание правка

--- src/test/java/fourth_lesson/ParametrizedLessonTest.java
+++ src/test/java/fourth_lesson/ParametrizedLessonTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static java.lang.Thread.sleep;
 
 public class ParametrizedLessonTest {
     SelenideElement input = $("input[name='sum']"),
                     submitButton = $("input[type='submit']"),
                     result = $("h3"),
+                    error = $("[name='error']"),
                     sum = $("span[name='sum']");
 
     String errorMessage = "Вы ввели не числовое значение.";
@@ -24,9 +26,11 @@ public class ParametrizedLessonTest {
 
     @ParameterizedTest(name = "Мой тест, #{index}, для значения: {0}")
     @ValueSource(strings = {"100", "2000", "йцукен"})
-    void setValueAndCalculate(String value) {
+    void setValueAndCalculate(String value) throws InterruptedException {
         input.setValue(value);
         submitButton.click();
+        sleep(1000);
+        Assertions.assertTrue(error.isDisplayed() ||sum.isDisplayed(), "Поле с информацией об ошибке или о результате расчета не отображается");
         Assertions.assertTrue(result.getText().contains(errorMessage)||sum.getText().contains(value));
     }
 }
