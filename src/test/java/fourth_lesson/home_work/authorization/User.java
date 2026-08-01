a079031c86db0e86bb1ce8876c9c808ace81c242
Dmitriy Sim <dmitriy.sim89@gmail.com>
2026-08-01 09:39:01 +0300

    4 урок ДЗ дополнение

--- src/test/java/fourth_lesson/home_work/authorization/User.java
+++ src/test/java/fourth_lesson/home_work/authorization/User.java
@@ -1,11 +1,8 @@
 package fourth_lesson.home_work.authorization;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class User {
 
-    private static final Logger log = LoggerFactory.getLogger(User.class);
     private String userName;
     private String password;
     private Boolean isBlocked;
@@ -42,7 +39,7 @@ public class User {
 
     public String checkAuth(String login, String password) {
         System.out.println("Проверка логина и пароля " + login + " и " + password +
-                " для пользователя " + this.userName + "");
+                " для пользователя " + this.userName);
         boolean authorization = login.equals(this.userName) && password.equals(this.password);
 
         if (isBlocked) {
--- src/test/java/fourth_lesson/home_work/authorization/UserTest.java
+++ src/test/java/fourth_lesson/home_work/authorization/UserTest.java
@@ -1,8 +1,10 @@
 package fourth_lesson.home_work.authorization;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -30,6 +32,22 @@ public class UserTest {
                 Arguments.of("user1", "password1",VALID_USERS.get(0)),
                 Arguments.of("user2", "password2",VALID_USERS.get(1)),
                 Arguments.of("user3", "password3",VALID_USERS.get(2)));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "users.csv", numLinesToSkip = 1)
+    @DisplayName("Проверка изменения данных пользователя")
+    void checkSetUser(String userName, String password, boolean isBlocked) {
+        User checkedUser = VALID_USERS.get(0);
+        checkedUser.setUserName(userName);
+        checkedUser.setPassword(password);
+        checkedUser.setBlocked(isBlocked);
+
+        boolean checkResult = checkedUser.getBlocked()==isBlocked&
+                checkedUser.getUserName().equals(userName)&
+                checkedUser.getPassword().equals(password);
+
+        Assertions.assertTrue(checkResult, "Один из параметров пользователя не изменился");
     }
 
     static Stream<Arguments> blockedUserProvider() {
